58e5c940718edeac4c44917b2db6ef1b7238d23c
Mirela <mkarabeg@gmail.com>
2017-02-18 13:17:00 +0100

    Add files via upload

--- Plot2.R
+++ Plot2.R
@@ -1,3 +1,11 @@
+#extracting file data 
+housedata <-read.table("household_power_consumption.txt", header=TRUE, 
+                       sep=";", na.strings = "?")
+
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
 
 ## Plot 2
 plot(data$Global_active_power~data$Datetime, type="l",
